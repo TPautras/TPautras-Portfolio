fb153816ec52942c0198afab5b7362894d5fa2ca
Thomas Pautras <tpautras@gmail.com>
2026-07-13 07:34:46 +0200

    feat: make starry night backdrop visible on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,17 @@
-import { PlanetCard } from "@/components/fallback/PlanetCards";
-import { PLANETS } from "@/lib/planets";
 import { headers } from "next/headers";
 import { isMobile } from "@/utils/isMobile";
+import MobilePortfolio from "@/components/fallback/MobilePortfolio";
 
 export default async function Home() {
   const userAgent = (await headers()).get("user-agent") || "";
   const mobileCheck = isMobile(userAgent);
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="flex flex-1 flex-col md:hidden">
-          {mobileCheck ? (PLANETS.map((element, i) => (
-            <div key={i} className="bg-amber-50 dark:bg-black">
-              <PlanetCard planet={element}/>
-            </div>
-          ))) : 
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+          {mobileCheck ?
+          <MobilePortfolio/> : 
           <div> test</div>
           }
-        </div>    
-        <div className="hidden lg:block">
-          Test
-        </div>
       </main>
     </div>
   );
--- components/fallback/MobilePortfolio.tsx
+++ components/fallback/MobilePortfolio.tsx
@@ -1,1 +1,17 @@
+import { PLANETS } from "@/lib/planets";
+import { PlanetCard } from "./PlanetCards";
+import { Starfield } from "./Starfield";
+
 //components/fallback/MobilePortfolio.tsx
+export default function MobilePortfolio() {
+    return (
+        <>
+        <Starfield/>
+        {PLANETS.map((element, i) => (
+            <div key={i}>
+              <PlanetCard planet={element}/>
+            </div>
+        ))}
+        </>
+    )
+}
